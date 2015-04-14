92e6ffb68d3f5e60b56c08c738c2c4243cecd8a1
jacob-wj <wangjia199055@126.com>
2015-04-14 10:45:09 +0800

    完成lesson1：实现一个随意滑动的view

--- app/src/main/java/com/jacob/viewdraghelper/MainActivity.java
+++ app/src/main/java/com/jacob/viewdraghelper/MainActivity.java
@@ -1,10 +1,14 @@
 package com.jacob.viewdraghelper;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.jacob.viewdraghelper.lesson1.LessonOneActivity;
 
 
 public class MainActivity extends FragmentActivity {
@@ -15,4 +19,20 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void lessonOne(View view){
+        Intent intent = new Intent(this, LessonOneActivity.class);
+        startActivity(intent);
+    }
+
+    public void lessonTwo(View view){
+
+    }
+
+    public void lessonThree(View view){
+
+    }
+
+    public void lessonFour(View view){
+
+    }
 }
