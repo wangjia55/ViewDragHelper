53b893a4a888f3cfd6a091b129a258402db6df2e
王佳 <wangjia@cvte.cn>
2015-04-15 17:40:29 +0800

    lesson5: 完成简易的滑动删除效果

--- app/src/main/java/com/jacob/viewdraghelper/MainActivity.java
+++ app/src/main/java/com/jacob/viewdraghelper/MainActivity.java
@@ -9,6 +9,7 @@ import com.jacob.viewdraghelper.lesson1.LessonOneActivity;
 import com.jacob.viewdraghelper.lesson2.LessonTwoActivity;
 import com.jacob.viewdraghelper.lesson3.LessonThreeActivity;
 import com.jacob.viewdraghelper.lesson4.LessonFourActivity;
+import com.jacob.viewdraghelper.lesson5.LessonFiveActivity;
 
 
 public class MainActivity extends FragmentActivity {
@@ -19,23 +20,29 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void lessonOne(View view){
+    public void lessonOne(View view) {
         Intent intent = new Intent(this, LessonOneActivity.class);
         startActivity(intent);
     }
 
-    public void lessonTwo(View view){
+    public void lessonTwo(View view) {
         Intent intent = new Intent(this, LessonTwoActivity.class);
         startActivity(intent);
     }
 
-    public void lessonThree(View view){
+    public void lessonThree(View view) {
         Intent intent = new Intent(this, LessonThreeActivity.class);
         startActivity(intent);
     }
 
-    public void lessonFour(View view){
+    public void lessonFour(View view) {
         Intent intent = new Intent(this, LessonFourActivity.class);
         startActivity(intent);
     }
+
+    public void lessonFive(View view) {
+        Intent intent = new Intent(this, LessonFiveActivity.class);
+        startActivity(intent);
+    }
+
 }
